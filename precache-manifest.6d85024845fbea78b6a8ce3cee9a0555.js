fdf1d8e72ec87a27ef5b3e1e6d846e8c8775be30
Svitlana Kryzhanivska <lana19333@gmail.com>
2020-03-19 17:27:02 +0200

    Updates

---
+++ precache-manifest.6d85024845fbea78b6a8ce3cee9a0555.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4692506ddd514ad1ee6c46c07d50a650",
+    "url": "/lana193/lizena.git/index.html"
+  },
+  {
+    "revision": "1c280aaff87d65aa63e2",
+    "url": "/lana193/lizena.git/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f13699b60558bfcec138",
+    "url": "/lana193/lizena.git/static/css/3.43ef3fcb.chunk.css"
+  },
+  {
+    "revision": "a3675f14adc0778f3697",
+    "url": "/lana193/lizena.git/static/css/4.43ef3fcb.chunk.css"
+  },
+  {
+    "revision": "9e9952ec0f575c53b8ee",
+    "url": "/lana193/lizena.git/static/css/main.0cfbd790.chunk.css"
+  },
+  {
+    "revision": "1c280aaff87d65aa63e2",
+    "url": "/lana193/lizena.git/static/js/2.da127101.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/lana193/lizena.git/static/js/2.da127101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13699b60558bfcec138",
+    "url": "/lana193/lizena.git/static/js/3.616bd792.chunk.js"
+  },
+  {
+    "revision": "a3675f14adc0778f3697",
+    "url": "/lana193/lizena.git/static/js/4.62b5770e.chunk.js"
+  },
+  {
+    "revision": "9e9952ec0f575c53b8ee",
+    "url": "/lana193/lizena.git/static/js/main.4f1c74c4.chunk.js"
+  },
+  {
+    "revision": "e0f90fd1c836c94c4718",
+    "url": "/lana193/lizena.git/static/js/runtime-main.ff0ca207.js"
+  },
+  {
+    "revision": "fd3ac6eda2921903aee36de5c580b2aa",
+    "url": "/lana193/lizena.git/static/media/background.fd3ac6ed.jpg"
+  },
+  {
+    "revision": "a60e4ff6236cc515c48258a257a01e71",
+    "url": "/lana193/lizena.git/static/media/logo.a60e4ff6.jpg"
+  },
+  {
+    "revision": "3be8e732d106eef5ca22745034fd3cd1",
+    "url": "/lana193/lizena.git/static/media/project-photo.3be8e732.jpg"
+  },
+  {
+    "revision": "9ee018380b738dd76448ddb4a47bcc98",
+    "url": "/lana193/lizena.git/static/media/project.9ee01838.jpg"
+  }
+]);
